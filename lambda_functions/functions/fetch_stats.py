befa110100a5d62efb4a578e9e18dd37a339defc
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2024-07-31 20:21:09 -0400

    Send data to s3 bucket

--- lambda_functions/functions/fetch_stats.py
+++ lambda_functions/functions/fetch_stats.py
@@ -5,15 +5,12 @@ import sys
 # Add the parent directory to the system path to find utils
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
-print(parent_dir)
 sys.path.append(parent_dir)
 
-from datetime import datetime, timedelta
-import pytz
 from utils.data_fetcher import FangraphsScraper
-# from utils.s3_uploader import upload_to_s3
+from utils.s3_uploader import upload_to_s3
 
-
+BUCKET_NAME = 'timjimmymlbdata'
 
 def lambda_handler(event, context):
     base_url_fb = "https://www.fangraphs.com/leaders/major-league"
@@ -24,8 +21,8 @@ def lambda_handler(event, context):
     url_barrel_hh = FangraphsScraper.generate_url(base_url_barrel_hh, "barrel_hh")
     data_barrel_hh = FangraphsScraper.get_or_fetch_data(url_barrel_hh, "barrel_hh", file_path="/tmp/fangraphs_barrel_hh_data.json")
 
-    # upload_to_s3('/tmp/fangraphs_fb_data.json', 'your-bucket-name', 'fangraphs_fb_data.json')
-    # upload_to_s3('/tmp/fangraphs_barrel_hh_data.json', 'your-bucket-name', 'fangraphs_barrel_hh_data.json')
+    upload_to_s3('./fangraphs_fb_data.json', BUCKET_NAME, 'fangraphs_fb_data.json')
+    upload_to_s3('./fangraphs_barrel_hh_data.json', BUCKET_NAME, 'fangraphs_barrel_hh_data.json')
 
     # Write to local for testing
     with open('./fangraphs_fb_data.json', 'w') as f:
